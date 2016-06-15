2854c71f99c199b9f0e6e1a12ec7aaa6244f11a3
Nagisao <ytgaowhu@gmail.com>
2016-06-15 11:52:16 +0800

    Add files via upload

---
+++ my_clock/js/my_clock.js
@@ -0,0 +1,13 @@
+function my_clock(e){
+	var today = new Date();
+	var h = today.getHours();
+	var m = today.getMinutes();
+	var s = today.getSeconds();
+	m = m>=10?m:('0'+m);
+	s = s>=10?s:('0'+s);
+	e.innerHTML = h+":"+m+":"+s;
+	setTimeout(function(){my_clock(e)},1000);
+}
+
+var clock_div = document.getElementById('clock_div');
+my_clock(clock_div);
